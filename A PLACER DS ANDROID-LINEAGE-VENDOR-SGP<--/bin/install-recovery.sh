db6a1227c120bec4dac81673cb903f50e8f75971
8bits-jpg <yufufmohamed@protonmail.com>
2021-01-15 16:01:17 +0400

    initial commit

--- A PLACER DS ANDROID-LINEAGE-VENDOR-SGP<--/bin/install-recovery.sh
+++
@@ -1,6 +0,0 @@
-#!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/bootdevice/by-name/recovery:14935332:3654adfaab237112409649352322de993b62254b; then
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/bootdevice/by-name/boot:14173472:9bc24fcf2d9862a7a07a03906d30eb1b01b32bbf EMMC:/dev/block/bootdevice/by-name/recovery 3654adfaab237112409649352322de993b62254b 14935332 9bc24fcf2d9862a7a07a03906d30eb1b01b32bbf:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
-else
-  log -t recovery "Recovery image already installed"
-fi
